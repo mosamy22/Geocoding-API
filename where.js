12a97764b84ae921a43e97e766731acc8d725eec
Mohamed Samy Hasan <mohamedsamyhasan486@gmail.com>
2019-08-27 14:02:49 -0400

    using dr\chuck api

---
+++ where.js
@@ -0,0 +1,13 @@
+myData = [
+[30.048465,31.45229, 'New Cairo 1st settlement, El yasmeen 1,Al Sayyeda Zeinab, محافظة القاهرة‬، Egypt'],
+[50.06688579999999,19.9136192, 'aleja Adama Mickiewicza 30, 30-059 Kraków, Poland'],
+[52.2394019,21.0150792, 'Krakowskie Przedmieście 5, 00-068 Warszawa, Poland'],
+[30.018923,31.499674, 'AUC Avenue، 11835, Egypt'],
+[33.4242399,-111.9280527, 'Tempe, AZ 85281, USA'],
+[38.0399391,23.8030901, 'Monumental Plaza, Building C, 1st Floor, Leof. Kifisias 44, Marousi 151 25, Greece'],
+[28.3588163,75.58802039999999, 'Vidya Vihar, Pilani, Rajasthan 333031, India'],
+[6.8946472,3.7174267, 'Ilishan-Remo, Nigeria'],
+[25.2677203,82.99125819999999, 'Ajagara, Varanasi, Uttar Pradesh 221005, India'],
+[12.9527314,77.5157387, 'Gnana Bharathi Campus, Gnana Bharathi Main Rd, Teachers Colony, Nagarbhavi, Bengaluru, Karnataka 560056, India'],
+[31.5497007,-97.1143046, '1301 S University Parks Dr, Waco, TX 76706, USA']
+];
